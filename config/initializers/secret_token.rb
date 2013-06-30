a99c1b1952bcaeb61d178f0c46897247c7c3e3b7
Robert Boone <robert@rlb3.com>
2013-06-30 15:43:06 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mediabox::Application.config.secret_key_base = '3d192a45649d5018adc4d014b618fa6a73a7546828f74c414e675b867a3cb434b0243a6c0619dd72fd0a74364704c9b22b602694567e79b3b3ef67fba9ea0b55'
